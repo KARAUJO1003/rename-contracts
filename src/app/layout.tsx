7d29052162596c8ed78d7776f27c4f5ce7a17361
Kaesyo Félix <129511975+KARAUJO1003@users.noreply.github.com>
2024-04-08 16:44:26 -0300

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "./providers/theme-provider";
+import { ModeToggle } from "./_components/toggle-theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            
+            {children}
+          </ThemeProvider>
+      <Toaster position="top-left" /></body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,170 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Copy } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ModeToggle } from "./_components/toggle-theme";
+
+interface RenamedProps {
+  quadra: number;
+  lote: number;
+  titulo: string;
+  venda: number;
+  id: number;
+}
 
 export default function Home() {
+  const [quadra, setQuadra] = useState(3);
+  const [lote, setLote] = useState(69);
+  const [titulo, setTitulo] = useState("CONTRATO");
+  const [venda, setVenda] = useState(0);
+  const [id, setId] = useState(1);
+  const [newFileName, setNewFileName] = useState<RenamedProps[]>([]);
+
+  function handleRenameFileName() {
+    setNewFileName([{ quadra, lote, titulo, venda, id }, ...newFileName]);
+  }
+
+  function handleCopyToClipboard(text: string) {
+    toast.success(text);
+    navigator.clipboard.writeText(text);
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex flex-col min-h-screen items-center justify-between p-24">
+      <Card className="w-full max-w-md relative">
+      <ModeToggle />
+        <CardHeader>
+          <CardTitle className="text-2xl">Renomeie Contratos</CardTitle>
+          <CardDescription>
+            Adicione as informações solicitadas abaixo e copie o resultado para
+            área de trabalho.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4 ">
+          <div className="grid gap-2">
+            <Label htmlFor="qd">
+              {" "}
+              <span className="text-xs text-red-500 mr-2">*</span>Quadra
+            </Label>
+            <Input
+              onKeyDown={(e) => e.key === "Enter" && handleRenameFileName()}
+              onChange={(e) => setQuadra(Number(e.target.value))}
+              value={quadra}
+              id="qd"
+              type="number"
+              placeholder="**"
+              required
             />
-          </a>
-        </div>
-      </div>
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="lt">
+              {" "}
+              <span className="text-xs text-red-500 mr-2">*</span>Lote
+            </Label>
+            <Input
+              onKeyDown={(e) => e.key === "Enter" && handleRenameFileName()}
+              onChange={(e) => setLote(Number(e.target.value))}
+              value={lote}
+              id="lt"
+              type="number"
+              placeholder="**"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="titulo">
+              {" "}
+              <span className="text-xs text-red-500 mr-2">*</span>Nome do
+              arquivo
+            </Label>
+            <Input
+              onKeyDown={(e) => e.key === "Enter" && handleRenameFileName()}
+              onChange={(e) => setTitulo(e.target.value)}
+              value={titulo}
+              id="titulo"
+              type="text"
+              placeholder="Digite o nome do arquivo..."
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="venda">
+              {" "}
+              <span className="text-xs text-red-500 mr-2">*</span>Venda
+            </Label>
+            <Input
+              onKeyDown={(e) => e.key === "Enter" && handleRenameFileName()}
+              onChange={(e) => setVenda(Number(e.target.value))}
+              value={venda}
+              id="venda"
+              type="number"
+              placeholder="Digite o número da venda..."
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="id">Indice</Label>
+            <Input
+              onKeyDown={(e) => e.key === "Enter" && handleRenameFileName()}
+              onChange={(e) => setId(Number(e.target.value))}
+              value={id}
+              id="id"
+              type="number"
+              placeholder="Digite o número da ordem do documento..."
+            />
+          </div>
+        </CardContent>
+        <CardFooter className="flex flex-col  gap-5">
+          <Button onClick={handleRenameFileName} className="w-full dark:bg-blue-600 dark:text-primary">
+            Gerar 
+          </Button>
+            <ScrollArea className="px-4">
+          <div className="max-h-40 space-y-3">
+            {newFileName.map((item, index) => (
+              <Card
+                className="p-3 w-full flex items-center justify-between shadow-none  space-x-3"
+                key={index}
+              >
+                <CardDescription>
+                  {`QD_${String(item.quadra).padStart(2, "0")}_LT_${String(
+                    item.lote
+                  ).padStart(2, "0")}_${item.titulo}_328_CDRVA_${item.venda}_(${
+                    item.id
+                  })`}
+                </CardDescription>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                <Button
+                  size={"icon"}
+                  variant={"outline"}
+                  onClick={() =>
+                    handleCopyToClipboard(
+                      `QD_${String(item.quadra).padStart(2, "0")}_LT_${String(
+                        item.lote
+                      ).padStart(2, "0")}_${item.titulo}_328_CDRVA_${
+                        item.venda
+                      }_(${item.id})`
+                    )
+                  }
+                >
+                  <Copy size={12} />
+                </Button>
+              </Card>
+            ))}
+          </div>
+            </ScrollArea>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
